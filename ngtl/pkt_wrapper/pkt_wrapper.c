c7f4d6632b72014370b0067bc82a6e22abc3e851
EngrArsalanPervez <usman.pervez10@gmail.com>
2025-11-16 01:15:14 +0500

    1

--- ngtl/pkt_wrapper/pkt_wrapper.c
+++ ngtl/pkt_wrapper/pkt_wrapper.c
@@ -54,7 +54,7 @@ void encapsulate_pkt(struct rte_mbuf **pkts, uint8_t nb_pkts, struct rte_mempool
                      uint16_t portid)
 {
     for (uint8_t i = 0; i < nb_pkts; i++) {
-        // print_pkt_rules(&active_rules[portid]);
+        print_pkt_rules(&active_rules[portid]);
         struct rte_mbuf *new_m = prepend_eth_ip_manual(pkts[i], pool, &active_rules[portid].src_mac,
                                                        &active_rules[portid].dst_mac,
                                                        active_rules[portid].src_ip,
