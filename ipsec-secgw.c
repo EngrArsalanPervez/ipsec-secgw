4a7d54884f2b1bbd9cb3efb7673d29f79e54268c
EngrArsalanPervez <usman.pervez10@gmail.com>
2025-11-15 19:36:03 +0500

    1

--- ipsec-secgw.c
+++ ipsec-secgw.c
@@ -363,7 +363,6 @@ struct ipsec_core_statistics core_statistics[RTE_MAX_LCORE];
 /* Print out statistics on packet distribution */
 static void print_stats_cb(__rte_unused void *param)
 {
-    return;
     uint64_t total_packets_dropped, total_packets_tx, total_packets_rx;
     float burst_percent, rx_per_call, tx_per_call;
     unsigned int coreid;
@@ -1517,7 +1516,6 @@ void ipsec_poll_mode_worker(void)
                 handle_packets(pkts, nb_rx, portid, lastPktTime);
 
                 if (client_ports_contains(portid)) {
-                    printf("Encapsulating pkt: %u\n", portid);
                     encapsulate_pkt(pkts, nb_rx, socket_ctx[0].mbuf_pool, portid);
                 }
 
