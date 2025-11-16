23be907cccaf6c80e0037191c6f359487a24c46d
EngrArsalanPervez <usman.pervez10@gmail.com>
2025-11-16 15:16:24 +0500

    1

--- ngtl/kni/kni.c
+++ ngtl/kni/kni.c
@@ -138,6 +138,8 @@ void kni_filter_ike_packets(int32_t nb_rx, struct rte_mbuf **pkts, uint16_t port
 
             appStatsData[TUNNEL_PORT].udpServicesData.udpTypeIKEv2++;
 
+            printf("TUNNEL_PORT:%u\n", tunnel_port);
+
             /* Burst tx to eth */
             uint8_t nb_tx = rte_eth_tx_burst(tunnel_port, 1, &m, 1);
             if (nb_tx)
