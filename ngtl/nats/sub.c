a3ba4263de87fbf99ab6e228bf256869ea0d323c
EngrArsalanPervez <usman.pervez10@gmail.com>
2025-11-16 16:56:42 +0500

    nats

--- ngtl/nats/sub.c
+++ ngtl/nats/sub.c
@@ -88,8 +88,10 @@ void *subscriber_thread(void *arg)
     }
 
 cleanup:
-    if (s != NATS_OK)
+    if (s != NATS_OK) {
         fprintf(stderr, "NATS error: %s\n", natsStatus_GetText(s));
+        exit(1);
+    }
 
     natsSubscription_Destroy(sub);
     natsConnection_Destroy(conn);
